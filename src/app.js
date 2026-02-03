2ab30ed50bbe6bf95ad0b98bd3ff52dc6e7f154b
Maxwell Bawa Tason <tasonmaxwell7t@gmail.com>
2026-02-03 23:32:40 +0000

    Fixed wrong file pasted in entry file

--- src/app.js
+++ src/app.js
@@ -22,7 +22,30 @@ app.use("/categories", categoriesRouter);
 app.use("/items", itemsRouter);
 /*
 app.get("/test-db", async (req, res) => {
-  const result = await pool.query("SELECT NOW()");
+  const result = a{
+  "name": "inventory_app",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node app.js",
+    "dev": "nodemon src/app.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "description": "",
+  "dependencies": {
+    "dotenv": "^17.2.3",
+    "ejs": "^4.0.1",
+    "express": "^5.2.1",
+    "pg": "^8.17.1"
+  },
+  "devDependencies": {
+    "nodemon": "^3.1.11"
+  }
+}
+wait pool.query("SELECT NOW()");
   res.send(result.rows[0]);
 });
 */
